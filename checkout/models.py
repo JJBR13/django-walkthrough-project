da7fb2384b522e2e4e5e8e51ef4a7be944f51d57
JJBR13 <jjbrown1308@gmail.com>
2023-03-08 13:39:15 +0000

    Create Checkout app & models

--- checkout/models.py
+++ checkout/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from products.models import Product
 
+
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -24,10 +25,52 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _gernerate_order_number(self):
+        """
+        Generate a random, unique order no. using UUTD
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Udate grand total, each time line is added
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['linitem_total_sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Overide OG save methods to set the order number, if has
+        not already
+        """
+
+        if not self.order_number:
+            self.order_number = self._gernerate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) #XS, S, M, L, XL
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantitiy = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Overide OG save methods to set the lineitem total and
+        update the order total
+        """
+        self.lineitem_total = self.product.price * self.quantitiy
+        super().save(*args, **kwargs)
+
+        def __str__(self):
+            return f'SKU {self.price.sku} on order {self.order_number}'
