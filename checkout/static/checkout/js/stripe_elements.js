2ef768dcb3d43fc252f97c44a44640a18203af58
JJBR13 <jjbrown1308@gmail.com>
2023-03-14 14:52:45 +0000

    Add loading overlay (stripe p9)

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,6 +53,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             // call card to stripe 
@@ -68,6 +70,9 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+
             // Allow card details to be update if error occured
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
