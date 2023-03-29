1a565bced9338df9d1697114e267af349d54f567
JJBR13 <jjbrown1308@gmail.com>
2023-03-29 08:28:26 +0000

    Updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,22 +20,23 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
+            
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or loclity',
         }
 
         # autofocus to True allows us to set where cursor will start
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # how a css class is set
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
