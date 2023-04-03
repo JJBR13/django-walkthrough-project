7dca9ea39bf74ab9b908643782b0f60ab80eabbc
JJBR13 <jjbrown1308@gmail.com>
2023-04-03 08:37:03 +0000

    Product Admin - add view,irl and template

--- products/views.py
+++ products/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
 
-# Create your views here.
+from .models import Product, Category
+from .form import ProductForm
 
 
 def all_products(request):
@@ -12,7 +12,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories= None
+    categories = None
     sort = None
     direction = None
 
@@ -70,3 +70,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
